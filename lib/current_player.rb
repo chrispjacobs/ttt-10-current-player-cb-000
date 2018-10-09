3aea2f0f18db94c536bc77f4cb66ccd7b48c9f8e
Christopher Jacobs <chrisj.3222@gmail.com>
2018-10-09 19:54:49 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,11 +2,19 @@
 def turn_count(board)
   counter = 1
     board.each do |turn|
-      if board[turn] = "X"
+      if turn = "X"
         counter += 1
-      elsif board[turn] = "O"
+      elsif turn = "O"
         counter += 1
       end
     end
     return counter
+end
+
+def current_player
+  if turn_count % 2 = 1
+    "X"
+  elsif turn_count % 2 = 0
+    "O"
+  end
 end
