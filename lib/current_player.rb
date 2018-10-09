1470a8c11643e00b5d6882e97cba23f47e7dd8c6
Christopher Jacobs <chrisj.3222@gmail.com>
2018-10-09 20:05:45 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,24 @@
+board = ["X", " ", " ", "O", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+  counter = 0
+    board.each do |turn|
+      if turn == "X"
+        counter += 1
+      elsif turn == "O"
+        counter += 1
+      end
+    end
+    return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  elsif turn_count(board) % 2 == 1
+    "O"
+  end
+end
+
+puts "The number of turns so far is #{turn_count(board)}."
+puts "The next turn's player should be #{current_player(board)}."
